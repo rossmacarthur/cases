1c9eb42eb18aae301a4d7b8fef7ae6f212478abd
Ross MacArthur <ross@macarthur.io>
2023-02-25 17:46:42 +0200

    Add implementation

--- cases.go
+++ cases.go
@@ -1,1 +1,180 @@
 package cases
+
+import (
+	"strings"
+	"unicode"
+)
+
+// ToCamel converts a string to camelCase.
+func ToCamel(s string) string {
+	first := true
+	writeFn := func(s *strings.Builder, word string) {
+		if first {
+			WriteLower(s, word)
+			first = false
+		} else {
+			WriteTitle(s, word)
+		}
+	}
+	return Transform(s, writeFn, DelimNone)
+}
+
+// ToPascal converts a string to PascalCase.
+func ToPascal(s string) string {
+	return Transform(s, WriteLower, DelimNone)
+}
+
+// ToSnake converts a string to snake_case.
+func ToSnake(s string) string {
+	return Transform(s, WriteLower, DelimUnderscore)
+}
+
+// ToScreamingSnake converts a string to SCREAMING_SKAKE_CASE.
+func ToScreamingSnake(s string) string {
+	return Transform(s, WriteUpper, DelimUnderscore)
+}
+
+// ToKebab converts a string to kebab-case.
+func ToKebab(s string) string {
+	return Transform(s, WriteLower, DelimHyphen)
+}
+
+// ToScreamingKebab converts a string to SCREAMING-KEBAB-CASE.
+func ToScreamingKebab(s string) string {
+	return Transform(s, WriteUpper, DelimHyphen)
+}
+
+// ToTitle converts a string to Title Case.
+func ToTitle(s string) string {
+	return Transform(s, WriteTitle, DelimNone)
+}
+
+// ToTrain converts a string to Train-Case.
+func ToTrain(s string) string {
+	return Transform(s, WriteTitle, DelimHyphen)
+}
+
+type state int
+
+const (
+	stateUnknown state = 0
+	stateDelims  state = 1
+	stateLower   state = 2
+	stateUpper   state = 3
+)
+
+type DelimFn = func(s *strings.Builder)
+
+type WriteFn = func(s *strings.Builder, word string)
+
+// Transform reconstructs the string using the given functions.
+//
+// writeFn is called for each word and delimFn is called for each word boundary.
+//
+// Word boundaries are defined as:
+// - we encounter a space, underscore or hyphen, consecutive are folded into one
+// - we encounter a transition from lowercase to uppercase
+// - we encounter the second last uppercase character
+func Transform(s string, writeFn WriteFn, delimFn DelimFn) string {
+	out := strings.Builder{}
+
+	runes := []rune(s)
+
+	// when we are on the first word
+	first := true
+	// the byte index of the start of the current word
+	start := 0
+	// the byte index of the end of the current word
+	end := -1
+	// the current state of the word boundary machine
+	state := stateUnknown
+
+	emit := func(end int) {
+		if end-start > 0 {
+			if !first {
+				delimFn(&out)
+			} else {
+				first = false
+			}
+			writeFn(&out, string(runes[start:end]))
+		}
+	}
+
+	for i, r := range runes {
+		if unicode.IsSpace(r) || r == '_' || r == '-' {
+			state = stateDelims
+			if end == -1 {
+				end = i // store the end of the previous word
+			}
+			continue
+		}
+
+		isLower := unicode.IsLower(r)
+		isUpper := unicode.IsUpper(r)
+
+		if state == stateDelims {
+			emit(end)
+			start = i
+			end = -1
+
+		} else if state == stateLower && isUpper {
+			emit(i)
+			start = i
+
+		} else if state == stateUpper && isUpper &&
+			i+1 < len(s) && unicode.IsLower(runes[i+1]) {
+			emit(i)
+			start = i
+		}
+
+		if isLower {
+			state = stateLower
+		} else if isUpper {
+			state = stateUpper
+		}
+	}
+
+	if state == stateDelims {
+		emit(end)
+	} else {
+		emit(len(runes))
+	}
+
+	return out.String()
+}
+
+// DelimNone is a delimiter function that does not insert any delimiter.
+func DelimNone(s *strings.Builder) {
+
+}
+
+// DelimUnderscore is a delimiter function that inserts an underscore.
+func DelimUnderscore(s *strings.Builder) {
+	s.WriteRune('_')
+}
+
+// DelimHyphen is a delimiter function that inserts a hyphen.
+func DelimHyphen(s *strings.Builder) {
+	s.WriteRune('-')
+}
+
+// WriteUpper writes the word in uppercase.
+func WriteUpper(s *strings.Builder, word string) {
+	s.WriteString(strings.ToUpper(word))
+}
+
+// WriteLower writes the word in lowercase.
+func WriteLower(s *strings.Builder, word string) {
+	s.WriteString(strings.ToLower(word))
+}
+
+// WriteTitle writes the word in title case.
+func WriteTitle(s *strings.Builder, word string) {
+	for i, r := range word {
+		if i == 0 {
+			s.WriteRune(unicode.ToUpper(r))
+		} else {
+			s.WriteRune(unicode.ToLower(r))
+		}
+	}
+}
--- cases_test.go
+++ cases_test.go
@@ -1,1 +1,206 @@
 package cases_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/rossmacarthur/cases"
+	"github.com/stretchr/testify/require"
+)
+
+type testCase struct {
+	in        string
+	snakeCase string
+	camelCase string
+}
+
+var tests []testCase = []testCase{
+	{},
+
+	{in: "Test",
+		snakeCase: "test",
+		camelCase: "test",
+	},
+
+	{in: "test case",
+		snakeCase: "test_case",
+		camelCase: "testCase",
+	},
+
+	{in: " test case",
+		snakeCase: "test_case",
+		camelCase: "testCase",
+	},
+
+	{in: "test case ",
+		snakeCase: "test_case",
+		camelCase: "testCase",
+	},
+
+	{in: "Test Case",
+		snakeCase: "test_case",
+		camelCase: "testCase",
+	},
+
+	{in: " Test Case",
+		snakeCase: "test_case",
+		camelCase: "testCase",
+	},
+
+	{in: "camelCase",
+		snakeCase: "camel_case",
+		camelCase: "camelCase",
+	},
+
+	{in: "PascalCase",
+		snakeCase: "pascal_case",
+		camelCase: "pascalCase",
+	},
+
+	{in: "snake_case",
+		snakeCase: "snake_case",
+		camelCase: "snakeCase",
+	},
+
+	{in: "SCREAMING_SNAKE_CASE",
+		snakeCase: "screaming_snake_case",
+		camelCase: "screamingSnakeCase",
+	},
+
+	{in: "kebab-case",
+		snakeCase: "kebab_case",
+		camelCase: "kebabCase",
+	},
+
+	{in: "SCREAMING-KEBAB-CASE",
+		snakeCase: "screaming_kebab_case",
+		camelCase: "screamingKebabCase",
+	},
+
+	{in: "Title Case ",
+		snakeCase: "title_case",
+		camelCase: "titleCase",
+	},
+
+	{in: "Train-Case ",
+		snakeCase: "train_case",
+		camelCase: "trainCase",
+	},
+
+	{in: "This is a Test case.",
+		snakeCase: "this_is_a_test_case.",
+		camelCase: "thisIsATestCase.",
+	},
+
+	{in: "MixedUP CamelCase, with some Spaces",
+		snakeCase: "mixed_up_camel_case,_with_some_spaces",
+		camelCase: "mixedUpCamelCase,WithSomeSpaces",
+	},
+
+	{in: "mixed_up_ snake_case with some _spaces",
+		snakeCase: "mixed_up_snake_case_with_some_spaces",
+		camelCase: "mixedUpSnakeCaseWithSomeSpaces",
+	},
+
+	{in: "this-contains_ ALLKinds OfWord_Boundaries",
+		snakeCase: "this_contains_all_kinds_of_word_boundaries",
+		camelCase: "thisContainsAllKindsOfWordBoundaries",
+	},
+
+	{in: "XΣXΣ baﬄe",
+		snakeCase: "xσxσ_baﬄe",
+		camelCase: "xσxσBaﬄe",
+	},
+
+	{in: "XMLHttpRequest",
+		snakeCase: "xml_http_request",
+		camelCase: "xmlHttpRequest",
+	},
+
+	{in: "FIELD_NAME11",
+		snakeCase: "field_name11",
+		camelCase: "fieldName11",
+	},
+
+	{in: "99BOTTLES",
+		snakeCase: "99bottles",
+		camelCase: "99bottles",
+	},
+
+	{in: "FieldNamE11",
+		snakeCase: "field_nam_e11",
+		camelCase: "fieldNamE11",
+	},
+
+	{in: "abc123def456",
+		snakeCase: "abc123def456",
+		camelCase: "abc123def456",
+	},
+
+	{in: "abc123DEF456",
+		snakeCase: "abc123_def456",
+		camelCase: "abc123Def456",
+	},
+
+	{in: "abc123Def456",
+		snakeCase: "abc123_def456",
+		camelCase: "abc123Def456",
+	},
+	{in: "abc123DEf456",
+		snakeCase: "abc123_d_ef456",
+		camelCase: "abc123DEf456",
+	},
+
+	{in: "ABC123def456",
+		snakeCase: "abc123def456",
+		camelCase: "abc123def456",
+	},
+
+	{in: "ABC123DEF456",
+		snakeCase: "abc123def456",
+		camelCase: "abc123def456",
+	},
+
+	{in: "ABC123Def456",
+		snakeCase: "abc123_def456",
+		camelCase: "abc123Def456",
+	},
+
+	{in: "ABC123DEf456",
+		snakeCase: "abc123d_ef456",
+		camelCase: "abc123dEf456",
+	},
+
+	{in: "ABC123dEEf456FOO",
+		snakeCase: "abc123d_e_ef456_foo",
+		camelCase: "abc123dEEf456Foo",
+	},
+
+	{in: "abcDEF",
+		snakeCase: "abc_def",
+		camelCase: "abcDef",
+	},
+
+	{in: "ABcDE",
+		snakeCase: "a_bc_de",
+		camelCase: "aBcDe",
+	},
+}
+
+func TestToCamelCase(t *testing.T) {
+	for _, tc := range tests {
+		t.Run(tc.in, func(t *testing.T) {
+			got := cases.ToCamel(tc.in)
+			require.Equal(t, tc.camelCase, got, fmt.Sprintf("'%s'", tc.in))
+		})
+	}
+}
+
+func TestToSnakeCase(t *testing.T) {
+	for _, tc := range tests {
+		t.Run(tc.in, func(t *testing.T) {
+			got := cases.ToSnake(tc.in)
+			require.Equal(t, tc.snakeCase, got, fmt.Sprintf("'%s'", tc.in))
+		})
+	}
+}
